ffa042837b2dc79f67a4e3bf7728fb7d054873fb
glmack <glmack@gmail.com>
2021-05-21 14:46:54 -0700

    Continue working through examples

--- iv_2.R
+++ iv_2.R
@@ -26,6 +26,30 @@ data("PSID1976")
 # summarize data set
 summary(PSID1976)
 
+# family background variables, such as father's education ('feducation') and mother's education ('meducation') are
+# possible candidates for IVs
+
+# regress log of wage on education, produces error
+model1 <- lm(log(wage) ~ education, data = PSID1976)
+
+#
+model2 <- lm(log(wage) ~ education, data = subset(PSID1976, participation=="yes"]
+
+summary(model2)
+
+plot(PSID1976$education[PSID1976$participation == "yes"],
+     log(PSID1976$wage[PSID1976$participation == "yes"],
+     col = "red", main = log(wage) vs education",
+     xlab = "education (years of schooling)",
+     ylab = "log(wage)")
+     
+abline(model2)
+
+# model 3
+model3 <- lm(education  ~ feducation,
+          data = subset(PSID1976, participation == 'yes))
+          
+summary(model3)
 
 
 
